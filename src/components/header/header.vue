4eb37714224c02bfd4bc867d399de58bf3c790f7
weibinzhu <fatbin37@hotmail.com>
2017-07-04 11:49:08 +0800

    添加了vue-router

---
+++ src/components/header/header.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="header">
+    i am header!!!!
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  export default {};
+</script>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import goods from 'components/goods/goods.vue';// './component'有别名
+import ratings from 'components/ratings/ratings.vue';
+import seller from 'components/seller/seller.vue';
+import './common/stylus/index.styl';
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
-})
+Vue.use(VueRouter);
+
+// 配置vue router
+let app = Vue.extend(App);
+
+let router = new VueRouter({
+  linkActiveClass: 'active'
+});
+
+router.map({
+  '/goods': {
+    component: goods
+  },
+  '/ratings': {
+    component: ratings
+  },
+  'seller': {
+    component: seller
+  }
+});
+
+router.start(app, '#app'); // 启动router
+router.go('/goods'); // 一进入页面就跳到goods
